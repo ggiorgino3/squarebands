8e65990d4a3deaa1d0708586d2710e03d89c1991
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-01-20 21:19:44 +0100

    Added News model, factory and seeder

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(50),
+            'content' => $this->faker->text(500),
+            'status' => $this->faker->randomElement(['publish', 'draft', 'trash']),
+        ];
+    }
+}
