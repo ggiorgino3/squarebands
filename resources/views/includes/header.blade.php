15676ab8e43015d3cf6121b25debda85d4d4e229
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-01-22 17:11:06 +0100

    Addes spacing for icons

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,9 @@
 <div class="navbar">
     <div class="navbar-inner">
-        <a id="logo" href="/">Dream Theater Logo</a>
+        <a id="logo" class="navbar-brand" href="/" >Dream Theater Logo</a>
         @foreach ($socials as $id => $url)
-            <a href="{{ $url }}" class="social" id="social_{{ $id }}">
-                <i class="fa-brands fa-{{$id}}"></i>
+            <a href="{{ $url }}" class="px-3 social" id="social_{{ $id }}">
+                <i class="social_icon fa-brands fa-{{$id}}"></i>
             </a>
         @endforeach
         <ul class="nav">
