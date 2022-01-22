a3269e79449810efeb14ca10a57e527e978f9bb9
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-01-22 18:17:10 +0100

    Added logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,11 @@
 <div class="navbar">
     <div class="navbar-inner">
-        <a id="logo" class="navbar-brand" href="/" >Dream Theater Logo</a>
+        <a id="logo" class="navbar-brand" href="/"> <img src="{{ asset('photos/logo.svg') }}" width="200px"
+                alt="Dream Theater Logo" class="d-inline-block align-middle mr-2">
+        </a>
         @foreach ($socials as $id => $url)
             <a href="{{ $url }}" class="px-3 social" id="social_{{ $id }}">
-                <i class="social_icon fa-brands fa-{{$id}}"></i>
+                <i class="social_icon fa-brands fa-{{ $id }}"></i>
             </a>
         @endforeach
         <ul class="nav">
