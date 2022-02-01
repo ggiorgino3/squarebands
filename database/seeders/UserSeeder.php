ed112ea8023a0b0632280fd1a1dcf384de0d8a17
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-02-01 13:10:56 +0100

    Aggiunto seeder con utente base

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $user = array(
+            'name' => "Gregorio Giorgino",
+            'email' => "giorgino.greg@gmail.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        );
+        User::create($user);
         User::factory()
             ->count(5)
             ->create();
