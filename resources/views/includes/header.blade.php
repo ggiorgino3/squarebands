bf15419fe6cf319883f6e39b1541fc7f2df6adee
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-02-12 17:04:32 +0100

    Restyling homepage

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,16 @@
 <div class="navbar">
-    <div class="navbar-inner">
+    <div class="navbar-inner w-100">
         <a id="logo" class="navbar-brand" href="/"> <img src="{{ asset('photos/logo.svg') }}" width="200px"
                 alt="Dream Theater Logo" class="d-inline-block align-middle mr-2">
         </a>
         @foreach ($socials as $id => $url)
-            <a href="{{ $url }}" class="px-3 social" id="social_{{ $id }}">
+            <a href="{{ $url }}" class="px-3 social float-end" id="social_{{ $id }}">
                 <i class="social_icon fa-brands fa-{{ $id }}"></i>
             </a>
         @endforeach
-        <ul class="nav">
+    </div>
+    <div class="row mx-5 w-100">
+        <ul class="nav my-4 justify-content-between">
             @foreach ($pages as $title => $url)
                 <li class="mx-4"><a href="/{{ $url }}">{{ $title }}</a></li>
             @endforeach
