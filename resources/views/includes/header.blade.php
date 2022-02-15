a917bc5336d1ac123fa75324624c89a0c866dac2
Gregorio Giorgino <giorgino.greg@gmail.com>
2022-02-15 09:43:49 +0100

    Fix menu homepage

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,16 +1,8 @@
 <div class="navbar mt-2">
-    <div class="navbar-inner  mt-2 w-100">
+    <div class="navbar-inner hstack mt-2 w-100">
         <a id="logo" class="navbar-brand" href="/"> <img src="{{ asset('photos/logo.svg') }}" width="200px"
                 alt="Dream Theater Logo" class="d-inline-block align-middle mr-2">
         </a>
-        <a href="{{ route('administration.homepage') }}"><img class="float-end mx-4 mt-2" src="{{ asset('assets/icon_admin.png') }}" width="100%" style="max-width: 30px" /></a>
-        @foreach ($socials as $id => $url)
-            <a href="{{ $url }}" class="px-3 social float-end" id="social_{{ $id }}">
-                <i class="social_icon fa-brands fa-{{ $id }}"></i>
-            </a>
-        @endforeach
-    </div>
-    <div class="row mx-5 w-100">
         <ul class="nav my-4 justify-content-between">
             <?php 
                 $parsedUrl = parse_url(URL::current());
@@ -24,6 +16,18 @@
                 <li class="mx-4"><a class="{{ isset($parsedUrl[1]) && $parsedUrl[1] == $url ? 'text-decoration-underline' : '' }}" href="/{{ $url }}">{{ $title }}</a></li>
             @endforeach
         </ul>
-    </div>
+        @foreach ($socials as $id => $url)
+            <a href="{{ $url }}" class="px-3 social float-end" id="social_{{ $id }}">
+                <i class="social_icon fa-brands fa-{{ $id }}"></i>
+            </a>
+        @endforeach
+        <div class="vr"></div>
+        <a href="{{ route('administration.homepage') }}"><img class="float-end ms-4" src="{{ asset('assets/icon_admin.png') }}" width="100%" style="max-width: 30px" /></a>
 
+    </div>
+{{--     <div class="row mx-5 w-100">
+ --}}        
+
+{{--     </div>
+ --}}
 </div>
