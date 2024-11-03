b1c51356ab69fe7dbab45eaf11656ddef3e2b2fe
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-11-03 20:01:07 +0100

    Fixed supplementary fields form

--- resources/views/includes/news/supplementary_fields.blade.php
+++ resources/views/includes/news/supplementary_fields.blade.php
@@ -1,8 +1,8 @@
 @extends('includes.basic_admin_form')
 @section('form_content')
     <div class="form-group required">
-        {{ Form::label('status', 'Status') }}
-        {{ Form::select('status', $statuses, Request::old('status', isset($model) ? $model->status : ''), ['required' => 'required']) }}
+        {{ html()->label('Status', 'status') }}
+        {{ html()->select('status', $statuses, Request::old('status', isset($model) ? $model->status : ''),) ->required() }}
     </div>
 @endsection
 
