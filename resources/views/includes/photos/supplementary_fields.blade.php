1cb107078d7ee318181fdd05b35889f0e4ad6966
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-11-03 20:50:37 +0100

    Fixed photo form

--- resources/views/includes/photos/supplementary_fields.blade.php
+++ resources/views/includes/photos/supplementary_fields.blade.php
@@ -2,36 +2,36 @@
 @section('form_content')
     <div class="form-group required">
         <div class="my-4">
-            {{ Form::label('caption', 'Caption', ['class' => 'd-block']) }}
+            {{ html()->label('caption', 'Caption',)->class('d-block') }}
             {{-- <h5><label for="caption">Caption</label> </h5> --}}
             {{-- <input required type="text" name="caption" id="caption" /> --}}
-            {{ Form::text('caption', Request::old('caption', isset($model) ? $model->caption : ''), ['required' => 'required', 'class' => 'w-50']) }}
+            {{ html()->text('caption', Request::old('caption', isset($model) ? $model->caption : ''))->required()->class('w-50') }}
         </div>
         <div class="my-4">
             <div>
-                {{ Form::label('photo', 'Photo') }}
+                {{ html()->label('Photo', 'photo') }}
             </div>
             @isset($model->uri)
                 <div class="w-50 my-2">
-                    <img loading="lazy" src="{{ $model->uri }}" class="w-100 image shadow-1-strong rounded mb-4" alt="{{ $model->name }}" />
+                    <img loading="lazy" src="{{ $model->uri }}" class="w-100 image shadow-1-strong rounded mb-4"
+                         alt="{{ $model->name }}"/>
                 </div>
             @endisset
-            {{ Form::file('photo', ['required' => 'required'] ) }}
+            {{ html()->file('photo')->required() }}
         </div>
     </div>
     <div class="form-group">
         <div class="my-2">
-            {{ Form::label('concert', 'Concert') }}
+            {{ html()->label('Concert', 'concert') }}
         </div>
         <div class="my-2">
-            {{ Form::select('concert', $concerts, Request::old('concert', isset($model) ? $model->concert_id : '')) }}
+            {{ html()->select('concert', $concerts, Request::old('concert', isset($model) ? $model->concert_id : '')) }}
         </div>
         <div class="my-2">
-            {{ Form::label('news', 'News') }}
+            {{ html()->label('News', 'news') }}
         </div>
         <div class="my-2">
-            {{ Form::select('news', $newses, Request::old('news', isset($model) ? $model->news_id : '')) }}
+            {{ html()->select('news', $newses, Request::old('news', isset($model) ? $model->news_id : '')) }}
         </div>
-       
     </div>
 @endsection
