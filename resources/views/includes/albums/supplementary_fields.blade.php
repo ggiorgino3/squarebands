7779cb1b90560a40ebe8c4c60ff6aafc95576184
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-11-03 20:25:31 +0100

    Fixed albums form

--- resources/views/includes/albums/supplementary_fields.blade.php
+++ resources/views/includes/albums/supplementary_fields.blade.php
@@ -2,13 +2,13 @@
 @section('form_content')
     <div class="form-group required">
         <div class="my-2">
-            {{ Form::label('genre', 'Genre') }}
-            {{ Form::select('genre',['Progressive Metal' => 'Progressive Metal', 'Hard Rock' => 'Hard Rock', 'Fusion' => 'Fusion'],Request::old('genre', isset($model) ? $model->genre : ''),['required' => 'required']) }}
+            {{ html()->label('Genre', 'genre') }}
+            {{ html()->select('genre',['Progressive Metal' => 'Progressive Metal', 'Hard Rock' => 'Hard Rock', 'Fusion' => 'Fusion'],Request::old('genre', isset($model) ? $model->genre : ''))->required() }}
         </div>
     </div>
     <div class="my-4">
-        {{ Form::label('publish_date', 'Publish Date', ['class' => 'px-0']) }}
-        {{ Form::text('publish_date', Request::old('publish_date', isset($model) ? $model->publish_date : ''), ['placeholder' => 'Format YYYY-MM-DD','required' => 'required']) }}
+        {{ html()->label('Publish Date', 'publish_date')->class('px-0') }}
+        {{ html()->text('publish_date', Request::old('publish_date', isset($model) ? $model->publish_date : ''))->placeholder('Format YYYY-MM-DD')->required() }}
     </div>
     @isset($model->songs)
         <h3>Related songs</h3>
