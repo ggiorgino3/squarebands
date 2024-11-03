39ea5d4ddd2cedd9425b3c54f3f7d00180a8ae8c
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-11-03 20:14:36 +0100

    Form songs fixed

--- resources/views/includes/songs/supplementary_fields.blade.php
+++ resources/views/includes/songs/supplementary_fields.blade.php
@@ -3,7 +3,7 @@
 
     <div class="form-group required">
         <div class="my-2">
-            {{ Form::label('song', 'Song') }}
+            {{ html()->label('Song', 'song') }}
         </div>
         <div class="my-2">
             @isset($model->uri)
@@ -11,40 +11,40 @@
                     <audio controls>
                         <source src="{{ $model->uri }}" type="audio/mpeg">
                         Your browser does not support the audio tag.
-                      </audio>
+                    </audio>
                 </div>
             @endisset
-            {{ Form::file('song', ['required' => 'required'] ) }}        
+            {{ html()->file('song', )->required() }}
         </div>
 
         <div class="my-2">
-            {{ Form::label('genre', 'Genre') }}
+            {{ html()->label('Genre', 'genre') }}
         </div>
         <div class="my-2">
-            {{ Form::select('genre', ['Progressive Metal', 'Hard Rock', 'Fusion'], Request::old('genre', isset($model) ? $model->genre : ''), ['required' => 'required']) }}
+            {{ html()->select('genre', ['Progressive Metal', 'Hard Rock', 'Fusion'], Request::old('genre', isset($model) ? $model->genre : ''))->required() }}
         </div>
     </div>
     <div class="form-group">
 
         <div class="my-2">
-            {{ Form::label('album', 'Album') }}
+            {{ html()->label('Album', 'album') }}
         </div>
         <div class="my-2">
-            {{ Form::select('album', $albums, Request::old('album', isset($model) ? $model->album_id : '')) }}
-        </div>
-
-        <div class="my-2">
-            {{ Form::label('duration', 'Duration') }}
-        </div>
-        <div class="my-2">
-            {{ Form::text('duration', Request::old('duration', isset($model) ? $model->duration : ''), ['placeholder' => "Format e.g. mm:ss"]) }}
+            {{ html()->select('album', $albums, Request::old('album', isset($model) ? $model->album_id : '')) }}
         </div>
 
         <div class="my-2">
-            {{ Form::label('tags', 'Song Tags') }}
+            {{ html()->label('Duration', 'duration') }}
+        </div>
+        <div class="my-2">
+            {{ html()->text('duration', Request::old('duration', isset($model) ? $model->duration : ''))->placeholder('Format e.g. mm:ss') }}
+        </div>
+
+        <div class="my-2">
+            {{ html()->label('Song Tags', 'tags') }}
         </div>
         <div class="my-2 ">
-            {{ Form::text('tags', Request::old('tags', isset($model) ? $model->tags : ''), ['placeholder' => 'Separate each tag with a comma', 'class' => 'w-100']) }}
+            {{ html()->text('tags', Request::old('tags', isset($model) ? $model->tags : ''))->placeholder('Separate each tag with a comma')->class('w-100') }}
         </div>
     </div>
 
