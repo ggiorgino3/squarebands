79666dbfc072d7e2f8c780443fa85f5fac140051
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-11-03 20:37:06 +0100

    Fixed concerts form

--- resources/views/includes/concerts/supplementary_fields.blade.php
+++ resources/views/includes/concerts/supplementary_fields.blade.php
@@ -2,36 +2,36 @@
 @section('form_content')
     <div class="form-group required">
         <div class="my-4">
-            {{ Form::label('place_name', 'Concert place name', ['class' => 'px-0']) }}
-            {{ Form::text('place_name', Request::old('place_name', isset($model) ? $model->place_name : ''), ['placeholder' => 'Stadium, Theater...','required' => 'required']) }}
+            {{ html()->label('Concert place name', 'place_name')->class('px-0') }}
+            {{ html()->text('place_name', Request::old('place_name', isset($model) ? $model->place_name : ''))->placeholder('Stadium, Theater...')->required() }}
         </div>
         <div class="my-4">
-            {{ Form::label('place_address', 'Concert place address', ['class' => 'px-0']) }}
-            {{ Form::text('place_address', Request::old('place_address', isset($model) ? $model->place_address : ''), ['placeholder' => 'Street...','required' => 'required']) }}
+            {{ html()->label('place_address', 'Concert place address')->class('px-0') }}
+            {{ html()->text('place_address', Request::old('place_address', isset($model) ? $model->place_address : ''))->placeholder('Street..')->required() }}
         </div>
         <div class="my-4">
-            {{ Form::label('country_name', 'Concert country', ['class' => 'px-0']) }}
-            {{ Form::text('country_name', Request::old('country_name', isset($model) ? $model->country_name : ''), ['placeholder' => 'Virginia, North Carolina','required' => 'required']) }}
+            {{ html()->label('Concert country','country_name')->class('px-0') }}
+            {{ html()->text('country_name', Request::old('country_name', isset($model) ? $model->country_name : ''))->placeholder('Virginia, North Carolina')->required()}}
         </div>
         <div class="my-4">
-            {{ Form::label('city_name', 'Concert city', ['class' => 'px-0']) }}
-            {{ Form::text('city_name', Request::old('city_name', isset($model) ? $model->city_name : ''), ['placeholder' => 'Sydney, Melbourne...','required' => 'required']) }}
+            {{ html()->label('Concert city', 'city_name')->class('px-0') }}
+            {{ html()->text('city_name', Request::old('city_name', isset($model) ? $model->city_name : ''))->placeholder('Sydney, Melbourne...')->required()}}
         </div>
         <div class="my-4">
-            {{ Form::label('datetime', 'Concert Date / Hour', ['class' => 'px-0']) }}
-            {{ Form::text('datetime', Request::old('datetime', isset($model) ? $model->datetime : ''), ['placeholder' => '','required' => 'required']) }}
+            {{ html()->label('Concert Date / Hour', 'datetime', )->class('px-0') }}
+            {{ html()->text('datetime', Request::old('datetime', isset($model) ? $model->datetime : ''))->required() }}
         </div>
     </div>
     <div class="my-4">
-        {{ Form::label('gate_opening', 'Gates opening hour', ['class' => 'px-0']) }}
-        {{ Form::text('gate_opening', Request::old('gate_opening', isset($model) ? $model->gate_opening : '')) }}
+        {{ html()->label('Gates opening hour', 'gate_opening')->class('px-0') }}
+        {{ html()->text('gate_opening', Request::old('gate_opening', isset($model) ? $model->gate_opening : '')) }}
     </div>
     <div class="my-4">
-        {{ Form::label('maximum_seating_no', 'Number of seatings', ['class' => 'px-0']) }}
-        {{ Form::text('maximum_seating_no', Request::old('maximum_seating_no', isset($model) ? $model->maximum_seating_no : '')) }}
+        {{ html()->label('Number of seatings','maximum_seating_no')->class('px-0') }}
+        {{ html()->text('maximum_seating_no', Request::old('maximum_seating_no', isset($model) ? $model->maximum_seating_no : '')) }}
     </div>
     <div class="my-4">
-        {{ Form::label('ticket_link', 'Ticket Website URL', ['class' => 'px-0']) }}
-        {{ Form::text('ticket_link', Request::old('ticket_link', isset($model) ? $model->ticket_link : '')) }}
+        {{ html()->label('ticket_link', 'Ticket Website URL')->class('px-0') }}
+        {{ html()->text('ticket_link', Request::old('ticket_link', isset($model) ? $model->ticket_link : '')) }}
     </div>
 @endsection
